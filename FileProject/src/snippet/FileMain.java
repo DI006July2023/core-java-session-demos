4f8cfbaafaa37dd829c0491429d1cf21a095bbfc
Poonga <bpoongs1982@gmail.com>
2023-08-11 11:40:42 -0400

    FileProject

---
+++ FileProject/src/snippet/FileMain.java
@@ -0,0 +1,18 @@
+package snippet;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileWriter fw = new FileWriter("orders\\Order_20100303.txt"); 
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
